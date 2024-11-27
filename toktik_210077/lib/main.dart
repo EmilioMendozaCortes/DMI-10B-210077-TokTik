d4a2d76f6008bbc54522d4606dd87246895ccfa0
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-11-26 23:50:06 -0600

    Practica 24
    Creación, Configuración y Modelos de Datos para la Aplicación de Reproducción Vertical de Videos

--- toktik_210077/lib/main.dart
+++ toktik_210077/lib/main.dart
@@ -5,14 +5,17 @@ import 'package:toktik_210077/config/presentation/providers/discover_provider.da
 import 'package:toktik_210077/config/presentation/screens/discover_screens.dart';
 
 void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => DiscoverProvider())],
+      providers: [
+        ChangeNotifierProvider(
+          lazy: false,
+          create: (_) => DiscoverProvider()..loadNextPage())
+      ],
       child: MaterialApp(
         title: 'TokTik 210077',
         debugShowCheckedModeBanner: false,
